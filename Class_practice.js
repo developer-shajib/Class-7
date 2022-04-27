d1052a2037e5468686a204f1467b2dbbe52896ad
shajib <sunhailshajib1@gmail.com>
2022-04-27 12:51:18 +0600

    Class 7 Assignment

---
+++ Class_practice.js
@@ -0,0 +1,46 @@
+
+// Class:7 part:1:-
+    //object data stracture,
+
+
+    
+// const data=[
+//     {
+//         name : "Asif",
+//         email: "asif@gmail.com"
+    
+//     },
+//     {
+//         name : "shajib",
+//         email: "developershajib@gmail.com"
+    
+//     },
+//     {
+//         name : "kamal",
+//         email: "kamal@gmail.com",
+//         result: {
+//             bn: 20,
+//             en: 32,
+//             math: 40,
+//             total: {
+//                 result: "A+ Paiso",
+//                 status: 'Happy'
+//             }
+//         }
+    
+//     },
+// ];
+// data.map((info,index)=>{
+//     if(info.name == "kamal"){
+
+    
+//     console.log(`${info.result.total.status}`)
+//     }
+// })
+
+// for (de of data){
+//     if(de.name == "kamal"){
+       
+//         console.log(de.result.total.status);
+//     }
+// }
---
+++ app.js
@@ -0,0 +1,375 @@
+
+////Create a Student Array & Object Data Structure ( name, roll, age, class, gender, location, Admission fees ) 30.  now create a console data table with
+
+
+
+// const stu_info=[{
+//     name : "shajib",
+//     roll : 23,
+//     age : 20,
+//     class : 3,
+//     gender : "M",
+//     location : "Cumilla",
+//     Admis_Fee : 200,
+// },{
+//     name : "Rakib",
+//     roll : 20,
+//     age : 30,
+//     class : 4,
+//     gender : "M",
+//     location : "Dhaka",
+//     Admis_Fee : 300,
+// },{
+//     name : "Hasan",
+//     roll : 64,
+//     age : 19,
+//     class : 5,
+//     gender : "M",
+//     location : "Barisal",
+//     Admis_Fee : 250,
+// },{
+//     name : "Lima",
+//     roll : 70,
+//     age : 64,
+//     class : 4,
+//     gender : "F",
+//     location : "Sylhet",
+//     Admis_Fee : 350,
+// },{
+//     name : "Forhad",
+//     roll : 25,
+//     age : 22,
+//     class : 3,
+//     gender : "M",
+//     location : "Khulna",
+//     Admis_Fee : 200,
+// },{
+//     name : "Fatema",
+//     roll : 60,
+//     age : 26,
+//     class : 5,
+//     gender : "F",
+//     location : "Dhaka",
+//     Admis_Fee : 340,
+// },
+// {
+//     name : "Jannat",
+//     roll : 43,
+//     age : 50,
+//     class : 4,
+//     gender : "F",
+//     location : "Cumilla",
+//     Admis_Fee : 260,
+// },{
+//     name : "Halima",
+//     roll : 63,
+//     age : 25,
+//     class : 3,
+//     gender : "F",
+//     location : "Sylhet",
+//     Admis_Fee : 400,
+// },{
+//     name : "Soykot",
+//     roll : 30,
+//     age : 28,
+//     class : 5,
+//     gender : "M",
+//     location : "Barisal",
+//     Admis_Fee : 350,
+// },{
+//     name : "Shovo",
+//     roll : 44,
+//     age : 26,
+//     class : 4,
+//     gender : "M",
+//     location : "Cumilla",
+//     Admis_Fee : 600,
+// },
+// {
+//     name : "Himel",
+//     roll : 44,
+//     age : 26,
+//     class : 4,
+//     gender : "M",
+//     location : "Cumilla",
+//     Admis_Fee : 600,
+// },{
+//     name : "Ayesha",
+//     roll : 44,
+//     age : 26,
+//     class : 4,
+//     gender : "F",
+//     location : "Cumilla",
+//     Admis_Fee : 600,
+// },{
+//     name : "Rafi",
+//     roll : 44,
+//     age : 26,
+//     class : 4,
+//     gender : "M",
+//     location : "Cumilla",
+//     Admis_Fee : 600,
+// },{
+//     name : "Samia",
+//     roll : 44,
+//     age : 26,
+//     class : 4,
+//     gender : "F",
+//     location : "Cumilla",
+//     Admis_Fee : 600,
+// },{
+//     name : "Helal",
+//     roll : 44,
+//     age : 26,
+//     class : 4,
+//     gender : "M",
+//     location : "Cumilla",
+//     Admis_Fee : 600,
+// },{
+//     name : "Oni",
+//     roll : 44,
+//     age : 26,
+//     class : 4,
+//     gender : "F",
+//     location : "Cumilla",
+//     Admis_Fee : 600,
+// },{
+//     name : "Toki",
+//     roll : 44,
+//     age : 26,
+//     class : 4,
+//     gender : "M",
+//     location : "Cumilla",
+//     Admis_Fee : 600,
+// },{
+//     name : "Santa",
+//     roll : 44,
+//     age : 26,
+//     class : 4,
+//     gender : "F",
+//     location : "Cumilla",
+//     Admis_Fee : 600,
+// },{
+//     name : "Nazmul",
+//     roll : 44,
+//     age : 26,
+//     class : 4,
+//     gender : "M",
+//     location : "Cumilla",
+//     Admis_Fee : 600,
+// },{
+//     name : "Nasima",
+//     roll : 44,
+//     age : 26,
+//     class : 4,
+//     gender : "F",
+//     location : "Cumilla",
+//     Admis_Fee : 600,
+// },{
+//     name : "Ashiq",
+//     roll : 44,
+//     age : 26,
+//     class : 4,
+//     gender : "M",
+//     location : "Cumilla",
+//     Admis_Fee : 600,
+// },{
+//     name : "Nila",
+//     roll : 44,
+//     age : 26,
+//     class : 4,
+//     gender : "F",
+//     location : "Cumilla",
+//     Admis_Fee : 600,
+// },{
+//     name : "Tanvir",
+//     roll : 44,
+//     age : 26,
+//     class : 4,
+//     gender : "M",
+//     location : "Cumilla",
+//     Admis_Fee : 600,
+// },{
+//     name : "Sreya",
+//     roll : 44,
+//     age : 26,
+//     class : 4,
+//     gender : "F",
+//     location : "Cumilla",
+//     Admis_Fee : 600,
+// },{
+//     name : "Sakib",
+//     roll : 44,
+//     age : 26,
+//     class : 4,
+//     gender : "M",
+//     location : "Cumilla",
+//     Admis_Fee : 600,
+// },{
+//     name : "Mim",
+//     roll : 44,
+//     age : 26,
+//     class : 4,
+//     gender : "F",
+//     location : "Cumilla",
+//     Admis_Fee : 600,
+// },{
+//     name : "Akhas",
+//     roll : 44,
+//     age : 26,
+//     class : 4,
+//     gender : "M",
+//     location : "Cumilla",
+//     Admis_Fee : 600,
+// },{
+//     name : "Kamal",
+//     roll : 44,
+//     age : 26,
+//     class : 4,
+//     gender : "M",
+//     location : "Cumilla",
+//     Admis_Fee : 600,
+// },{
+//     name : "Jamal",
+//     roll : 44,
+//     age : 26,
+//     class : 4,
+//     gender : "M",
+//     location : "Cumilla",
+//     Admis_Fee : 600,
+// },{
+//     name : "Nayem",
+//     roll : 44,
+//     age : 26,
+//     class : 4,
+//     gender : "M",
+//     location : "Cumilla",
+//     Admis_Fee : 600,
+// },
+// ];
+
+
+
+////Total Admnission fees
+
+// let total= 0;
+// stu_info.forEach((data,index)=>{
+//    console.log(`Name : ${data.name}---Ad-Fee : ${data.Admis_Fee}`)
+//    total += +data.Admis_Fee
+// })
+
+// console.log(`=====================
+// Total Fee   : ${total}`)
+
+
+////Find All Female Students
+
+// for(data of stu_info){
+//     if(data.gender == "F"){
+//         console.log(`
+//         Name: ${data.name}
+//         Roll: ${data.roll}
+//         Class: ${data.class}
+//         Age : ${data.age}
+//         Gender: ${data.gender}
+//         Location: ${data.location}
+//         Admis_Fee: ${data.Admis_Fee}`);
+//     }
+// };
+
+
+
+////Find class wise student result
+
+// stu_info.map((data,index)=>{
+//     if(data.class == "4"){
+//         console.log(`
+//         Name: ${data.name}
+//         Roll: ${data.roll}
+//         Class: ${data.class}
+//         Age : ${data.age}
+//         Gender: ${data.gender}
+//         Location: ${data.location}
+//         Admis_Fee: ${data.Admis_Fee}`);
+//     }
+// });
+
+////Location wise student result
+
+// stu_info.sort().forEach((data,index)=>{
+//     if(data.location == "Cumilla" || data.location == "Sylhet"){
+//         console.log(`
+//         Name: ${data.name}
+//         Roll: ${data.roll}
+//         Class: ${data.class}
+//         Age : ${data.age}
+//         Gender: ${data.gender}
+//         Location: ${data.location}
+//         Admis_Fee: ${data.Admis_Fee}`);
+       
+//     }
+// })
+
+////find student between 10 - 25 age
+
+// for(data of stu_info){
+//     if(data.age >= 10 && data.age <= 25){
+//             console.log(`
+//         Name: ${data.name}
+//         Roll: ${data.roll}
+//         Class: ${data.class}
+//         Age : ${data.age}
+//         Gender: ${data.gender}
+//         Location: ${data.location}
+//         Admis_Fee: ${data.Admis_Fee}`);
+       
+//     }
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// [
+//     ["Shajib","40","34","3","M","Cumilla","300"],
+//     ["Rakib","20","30","4","M","Dhaka","420"],
+//     ["Hasan","64","19","5","M","Barisal","430"],
+//     ["Lima","70","64","4","F","Sylhet","320"],
+//     ["Forhad","25","22","3","M","Khulna","200"],
+//     ["Fatema","60","26","5","F","Dhaka","340"],
+//     ["Jannat","43","50","4","F","Cumilla","700"],
+//     ["Soykot","30","28","5","M","Barisal","500"],
+//     ["Halima","63","25","3","F","Sylhet","400"],
+//     ["Shovo","44","26","4","M","Cumilla","600"],
+//     ["Rokib","40","34","3","M","Cumilla","300"],
+//     ["Ahsam","20","30","4","M","Dhaka","420"],
+//     ["Faruk","64","19","5","M","Barisal","430"],
+//     ["Nasima","70","64","4","F","Sylhet","320"],
+//     ["Sharif","25","22","3","M","Khulna","200"],
+//     ["Khadija","60","26","5","F","Dhaka","340"],
+//     ["Asma","43","50","4","F","Cumilla","700"],
+//     ["Nazmul","30","28","5","M","Barisal","500"],
+//     ["Sumiyya","63","25","3","F","Sylhet","400"],
+//     ["Sakib","44","26","4","M","Cumilla","600"],
+//     ["Maruf","40","34","3","M","Cumilla","300"],
+//     ["Nayem","20","30","4","M","Dhaka","420"],
+//     ["Iqbal","64","19","5","M","Barisal","430"],
+//     ["Santa","70","64","4","F","Sylhet","320"],
+//     ["Sakil","25","22","3","M","Khulna","200"],
+//     ["Daliya","60","26","5","F","Dhaka","340"],
+//     ["Sumi","43","50","4","F","Cumilla","700"],
+//     ["Rezaul","30","28","5","M","Barisal","500"],
+//     ["Tamanna","63","25","3","F","Sylhet","400"],
+//     ["Halim","44","26","4","M","Cumilla","600"],
+// ];
